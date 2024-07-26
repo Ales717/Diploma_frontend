742d4d0e6cea2324fccf2dcfe9675c3af6f226c6
Ales717 <prosenjak.ales77@gmail.com>
2024-07-26 11:10:19 +0200

    first commit

---
+++ src/hooks/usePageIdentification.ts
@@ -0,0 +1,51 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+const onDefault = () => {
+  document.title = 'diploma'
+  document.body.id = ''
+}
+const onHome = () => {
+  document.title = 'diploma'
+  document.body.id = 'home-page'
+}
+
+const onLogin = () => {
+  document.title = 'diploma - Login'
+  document.body.id = 'login-page'
+}
+const onSignup = () => {
+  document.title = 'diploma - Signup'
+  document.body.id = 'signup-page'
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const callbacks: any = {
+  '/': [onHome],
+  '/login': [onLogin],
+  '/signup': [onSignup],
+  '*': [onDefault],
+}
+
+export const addPageIdentification = (_case: string, fn: () => void) => {
+  callbacks[_case] = callbacks[_case] || []
+  callbacks[_case].push(fn)
+}
+
+export const usePageIdentification = () => {
+  const location = useLocation()
+
+  const customSwitch = (value: string) => {
+    if (callbacks[value]) {
+      callbacks[value].forEach((fn: () => void) => {
+        fn()
+      })
+    } else {
+      onDefault()
+    }
+  }
+
+  useEffect(() => {
+    if (location.pathname) customSwitch(location.pathname)
+  }, [location.pathname])
+}
